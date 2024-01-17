ed0a5473608ac2932444961668c530ba2e38f993
Ashish Varsani <ashishvarsani3@gmail.com>
2024-01-17 16:18:05 +0530

    practice of string function

---
+++ str_functtion.php
@@ -0,0 +1,41 @@
+<?php
+  
+ // echo strlen("ashish");
+
+
+  //echo str_replace("hello","ashish","hello word");
+  
+  
+  //echo strpos("my name is ashish","ashish");
+
+
+  //echo substr("i am ashish",4,11);
+
+
+  //echo strtolower("ASHISH");
+
+
+ // echo strtoupper("ashish");
+
+
+  //echo trim("      My Name Is Ashish         ");
+
+
+ /* $arr=array('a','b','c','d');
+  $str="++";
+  echo implode($str,$arr);*/
+
+
+ // print_r (explode(" ","my name is ashish"));
+
+
+// echo htmlspecialchars("this work <br> is awsome");
+
+/* $str = '<a href="https://xyz.com</a>';
+echo htmlentities($str); */
+
+
+echo str_repeat("Ashish ",3);
+
+   
+?>
