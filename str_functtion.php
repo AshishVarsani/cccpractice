eb6877b78ff5adfd9de425e34731b46c784a12a2
Ashish Varsani <ashishvarsani3@gmail.com>
2024-01-17 18:05:58 +0530

    string function practice4

--- str_functtion.php
+++ str_functtion.php
@@ -1,10 +1,11 @@
 <?php
   
  // echo strlen("ashish");
+ // return length of string
 
 
   //echo str_replace("hello","ashish","hello word");
-  
+  // replace the given string to
   
   //echo strpos("my name is ashish","ashish");
 
@@ -16,28 +17,64 @@
 
 
  // echo strtoupper("ashish");
-
+//
 
   //echo trim("      My Name Is Ashish         ");
-
+  // remove the white space from front and end of the string.
 
  /* $arr=array('a','b','c','d');
   $str="++";
   echo implode($str,$arr);*/
-
+  //Joins array elements with a string.
 
  // print_r (explode(" ","my name is ashish"));
-
+//split string into array
 
 // echo htmlspecialchars("this work <br> is awsome");
+//Converts special characters to HTML entities.
 
 /* $str = '<a href="https://xyz.com</a>';
 echo htmlentities($str); */
-
+//Converts all applicable characters to HTML entities
 
  //echo str_repeat("Ashish ",3);
+//repeat the string in given time
 
- echo "hi";
+// echo strrev("hi");
+//reverse the given string
 
-   
+//echo str_shuffle("Ashish");
+//randomly shuffle all char. of string
+
+//print_r(str_split("my name is ashish",4));
+//split the string to array for given length
+
+//echo str_word_count("my name is ashish");
+//Returns the number of words in a string.
+ 
+//echo substr_replace("i love myself","php",7);
+//Replaces a portion of a string with another string.
+
+//$str="Ashish";
+//echo str_pad($str,12,'*',STR_PAD_LEFT);
+//echo str_pad($str,12,'*',STR_PAD_RIGHT);
+//echo str_pad($str,12,'*',STR_PAD_BOTH);
+//Pads a string to a certain length with another string.
+
+//$str1="ashish";
+//$str2="kashis";
+//echo strcoll($str1,$str2);
+//locale based string comparison.
+
+//echo strcspn("Ashish", "h",0, 5);
+//Finds the length of the initial segment not matching a mask.
+
+//echo stristr("Hello world!","WORLD");
+//Case-insensitive search for the first occurrence of a string.
+
+//echo ucfirst("my name is ashish");
+//Converts the first character of a string to uppercase.
+
+//echo ucwords("my name is ashish");
+//Converts the first character of each word in a string to uppercase.
 ?>
