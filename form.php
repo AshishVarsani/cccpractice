8c7b5e74c032ade4a8933090261840a7e3516ddb
Ashish Varsani <ashishvarsani3@gmail.com>
2024-01-25 09:34:33 +0530

    function practice

--- form.php
+++ form.php
@@ -6,19 +6,19 @@
 <body>
     <form action="form_backend.php" method="post">
                 
-                    <input type="text" class="input" name="product_name" placeholder="Product Name" id="product_name" required pattern="[a-zA-Z'-'\s]*"> <br><br>
-                    <input type="text" class="input" name="sku" placeholder="SKU" id="sku" required pattern="[a-zA-Z'-'\s]*">
+                    <input type="text" class="input" name="ccc_product[product_name]" placeholder="Product Name" id="product_name" required pattern="[a-zA-Z'-'\s]*"> <br><br>
+                    <input type="text" class="input" name="ccc_product[sku]" placeholder="SKU" id="sku" required pattern="[a-zA-Z'-'\s]*">
                 <br><br>
                     <label for="product_type">Product Type</label>
                     <div class="radio-group">
-                        <input type="radio" id="simple" name="product_type" value="simple" checked>
+                        <input type="radio" id="simple" name="ccc_product[product_type]" value="simple" checked>
                         <label for="simple">Simple</label>
-                        <input type="radio" id="bundle" name="product_type" value="bundle">
+                        <input type="radio" id="bundle" name="ccc_product[product_type]" value="bundle">
                         <label for="bundle">Bundle</label>
 
                 <br><br>
-                    <label name="category">Category:</label>
-                    <select id="category" name="category" class="input">
+                    <label name="ccc_product[category]">Category:</label>
+                    <select id="category" name="ccc_product[category]" class="input">
                         <option value="bgr">Bar & Game Room</option>
                         <option value="bedroom">Bedroom</option>
                         <option value="decor">Decor</option>
@@ -30,10 +30,10 @@
                         <option value="outdoor">Outdoor</option>
                     </select>
                 <br><br>
-                    <input type="text" class="input" name="manufacturer_cost" placeholder="Manufacturer Cost" id="manufacturer_cost"> <br><br>
-                    <input type="text" class="input" name="shipping_cost" placeholder="Shipping Cost" id="shipping_cost"> <br><br>
-                    <input type="text" class="input" name="total_cost" placeholder="Total Cost" id="total_cost"> <br><br>
-                    <input type="text" class="input" name="price" placeholder="Price" id="price"> <br><br>
+                    <input type="text" class="input" name="ccc_product[manufacturer_cost]" placeholder="Manufacturer Cost" id="manufacturer_cost"> <br><br>
+                    <input type="text" class="input" name="ccc_product[shipping_cost]" placeholder="Shipping Cost" id="shipping_cost"> <br><br>
+                    <input type="text" class="input" name="ccc_product[total_cost]" placeholder="Total Cost" id="total_cost"> <br><br>
+                    <input type="text" class="input" name="ccc_product[price]" placeholder="Price" id="price"> <br><br>
 
                     <label for="status">Status:</label>
                     <select id="status" name="status" class="input">
@@ -42,9 +42,9 @@
                     </select>
                 <br><br>
                 <label for="created_at">Created At:</label>
-                    <input type="date" class="input" name="created_at" placeholder="Created At" id="created_at"> <br><br>
+                    <input type="date" class="input" name="ccc_product[created_at]" placeholder="Created At" id="created_at"> <br><br>
                 <label for="updated_at">Updated At:</label>
-                    <input type="date" class="input" name="updated_at" placeholder="Updated At" id="updated_at"> <br><br>
+                    <input type="date" class="input" name="ccc_product[updated_at]" placeholder="Updated At" id="updated_at"> <br><br>
                     <input type="submit" class="submit" value="Submit" id="submit" name="submit">
     </form>
 </body>
--- form_backend.php
+++ form_backend.php
@@ -1,21 +1,17 @@
 <?php 
+
+include("php_function.php");
+include("mysql_function.php");
 $conn = mysqli_connect("localhost","root","","ccc_practice");
 
-$product_name=$_POST["product_name"];
-$sku=$_POST["sku"];
-$product_type=$_POST["product_type"];
-$category=$_POST["category"];
-$manufacturer_cost=$_POST["manufacturer_cost"];
-$shipping_cost=$_POST["shipping_cost"];
-$total_cost=$_POST["total_cost"];
-$price=$_POST["price"];
-$status=$_POST["status"];
-
-$sql="INSERT INTO `ccc_product` (`product_name`, `sku`, `product_type`, `category`, `manufacturer_cost`, `shipping_cost`, `total_cost`, `price`, `status`)
-VALUES ('$product_name', '$sku', '$product_type', '$category', '$manufacturer_cost','$shipping_cost','$total_cost','$price','$status')";
+$data=getPostData("ccc_product");
+echo "<pre>";
+print_r($data);
+$sql=insert("ccc_product",$data);
 $insert=mysqli_query($conn,$sql);
 if($insert){
-    echo "Data add successfully";
+    echo "<script>alert('Data add successfully')</script>";
+    echo "<script>location. href='form.php'</script>";
 }else {
     echo "error";
 }
